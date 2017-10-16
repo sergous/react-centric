2f534d2df0db86113bc8ae5ee65c8946734e008d
Sergey Smirnov <s@slctd.com>
2017-10-16 21:10:59 +0000

    Merged in setup-react-router-redux (pull request #10)
    Setup react router redux
    
    * install react-router-redux.
    
    * add router-reducer to store.
    
    * sync browser-history with store.

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -4,11 +4,13 @@ import UserReducer from './user';
 import ProfileReducer from './profile';
 import SettingsReducer from './settings';
 import AuthReducer from './auth';
+import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
     user: UserReducer,
     profile: ProfileReducer,
     settings: SettingsReducer,
     form: FormReducer,
-    auth: AuthReducer
+    auth: AuthReducer,
+    routing: routerReducer
 });
