6ed45a03334ed6b28dc6cda18daf6c3d18599aad
Sergei Smirnov <sergous@gmail.com>
2018-11-13 23:45:07 -0800

    Add tasks sidebar.

--- app/components/Pages/Tasks.jsx
+++ app/components/Pages/Tasks.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import pubsub from 'pubsub-js'
-import TaskListItem from './TaskListItem'
+import { Grid, Row, Col, Modal, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
-import * as TASK_ACTIONS from '../../actions/task'
 import Nestable from 'react-nestable'
+import * as TASK_ACTIONS from '../../actions/task'
+import TaskListItem from './TaskListItem'
+import TaskForm from './TaskForm'
 
 import './Tasks.scss'
 import { bindActionCreators } from 'redux'
@@ -13,16 +15,16 @@ class Tasks extends React.Component {
         pubsub.publish('setPageTitle', this.constructor.name)
     }
 
-    renderTask({ item: task, collapseIcon }) {
-        return (
+    renderTask({ openTask }) {
+        return ({ item: task, collapseIcon }) =>
             task && (
                 <TaskListItem
                     task={task}
                     key={task.id}
+                    onOpen={openTask}
                     collapseIcon={collapseIcon}
                 />
             )
-        )
     }
 
     renderCollapseIcon({ isCollapsed }) {
@@ -31,48 +33,161 @@ class Tasks extends React.Component {
     }
 
     render() {
+        const newTask = {}
         const {
             tasks,
             isCollapsed,
-            Actions: { reorderTasks, collapseTasks },
+            isNew,
+            editingTask,
+            openTask,
+            Actions,
         } = this.props
+
+        const addTaskModal = isNew ? (
+            <Modal
+                show={editingTask !== null}
+                onHide={() => Actions.closeTask()}
+                className="modal-right modal-auto-size"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>New Task</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <TaskForm
+                        initialValues={newTask}
+                        editTaskProp={Actions.editTaskProp}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => Actions.closeTask()}>Cancel</Button>
+                    <Button
+                        className="btn btn-primary"
+                        onClick={() => Actions.addTask(editingTask)}
+                    >
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        ) : null
+
+        const editTaskModal =
+            editingTask && !isNew ? (
+                <Modal
+                    show={editingTask !== null}
+                    onHide={() => Actions.closeTask()}
+                    className="modal-right modal-auto-size"
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Project</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <TaskForm
+                            initialValues={editingTask}
+                            editProjectProp={Actions.editTaskProp}
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => Actions.openTask(editingTask)}>
+                            Cancel
+                        </Button>
+                        <Button
+                            onClick={() => Actions.removeTask(editingTask.id)}
+                        >
+                            Remove
+                        </Button>
+                        <Button
+                            className="btn btn-primary"
+                            onClick={() =>
+                                Actions.updateTask(editingTask.id, editingTask)
+                            }
+                        >
+                            Save
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            ) : null
+
+        let openTaskModal = null
+        if (openTask) {
+            openTaskModal = (
+                <Modal
+                    show={openTask !== null}
+                    onHide={() => Actions.closeTask()}
+                    className="modal-right modal-auto-size"
+                >
+                    <Modal.Header closeButton />
+                    <Modal.Body>
+                        <h3>{openTask.name}</h3>
+                        <h4>{openTask.description}</h4>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => Actions.closeTask()}>
+                            Close
+                        </Button>
+                        <Button onClick={() => Actions.editTask(openTask)}>
+                            Edit
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            )
+        }
+
+        const taskModal = addTaskModal || editTaskModal || openTaskModal
+
         return (
             <section>
-                <div className="container-fluid">
-                    <div className="js-nestable-action">
-                        <a
-                            onClick={() => collapseTasks(true)}
-                            className="btn btn-default btn-sm mr-sm"
-                        >
-                            Expand All
-                        </a>
-                        <a
-                            onClick={() => collapseTasks(false)}
-                            className="btn btn-default btn-sm"
-                        >
-                            Collapse All
-                        </a>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
+                {taskModal}
+                <Grid fluid>
+                    <Row className="mv">
+                        <Col sm={6}>
+                            <button
+                                type="button"
+                                className="btn btn-info"
+                                onClick={() => Actions.createTask(newTask)}
+                            >
+                                <em className="ion-plus mr-sm" />
+                                Add Task
+                            </button>
+                        </Col>
+                        <Col sm={6}>
+                            <div className="js-nestable-action pull-right">
+                                <a
+                                    onClick={() => Actions.collapseTasks(true)}
+                                    className="btn btn-default btn-sm mr-sm"
+                                >
+                                    Expand All
+                                </a>
+                                <a
+                                    onClick={() => Actions.collapseTasks(false)}
+                                    className="btn btn-default btn-sm"
+                                >
+                                    Collapse All
+                                </a>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
                             <div id="nestable" className="dd">
                                 <ol className="dd-list">
                                     <Nestable
                                         collapsed={isCollapsed}
                                         onChange={updTasks =>
-                                            reorderTasks(updTasks)
+                                            Actions.reorderTasks(updTasks)
                                         }
                                         items={tasks}
-                                        renderItem={this.renderTask}
+                                        renderItem={this.renderTask({
+                                            openTask: Actions.openTask,
+                                        })}
                                         renderCollapseIcon={
                                             this.renderCollapseIcon
                                         }
                                     />
                                 </ol>
                             </div>
-                        </div>
-                    </div>
-                </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </section>
         )
     }
@@ -82,6 +197,9 @@ const mapStateToProps = state => {
     return {
         tasks: state.task.tasks,
         isCollapsed: state.task.isCollapsed,
+        isNew: state.task.isNew,
+        editingTask: state.task.editingTask,
+        openTask: state.task.openTask,
     }
 }
 
--- app/reducers/task.jsx
+++ app/reducers/task.jsx
@@ -1,13 +1,144 @@
-import * as TYPES from '../actiontypes/task';
-import { initialState } from '../constants/task';
+import * as TYPES from '../actiontypes/task'
+import { actionTypes as FORM_TYPES } from 'redux-form'
+import { initialState } from '../constants/task'
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case TYPES.REORDER_TASKS:
-            return { ...state, tasks: action.payload };
+            return { ...state, tasks: action.payload }
         case TYPES.COLLAPSE_TASKS:
-            return { ...state, isCollapsed: action.payload };
+            return { ...state, isCollapsed: action.payload }
+        case TYPES.CREATE_TASK:
+            return {
+                ...state,
+                editingTask: { ...action.task },
+                editingProp: null,
+                isNew: true,
+            }
+        case TYPES.ADD_TASK:
+            return {
+                ...state,
+                nextId: state.nextId + 1,
+                tasks: [...state.tasks, { ...action.task, id: state.nextId }],
+                editingTask: null,
+                isNew: false,
+            }
+        case TYPES.EDIT_TASK_PROP:
+            return {
+                ...state,
+                editingTask: {
+                    ...state.editingTask,
+                    [action.field]: action.payload,
+                },
+            }
+        case FORM_TYPES.CHANGE:
+            if (action.meta.form !== 'task') return state
+
+            if (state.editingTask) {
+                const editingTask = { ...state.editingTask }
+                editingTask[action.meta.field] = action.payload
+                return {
+                    ...state,
+                    editingTask,
+                }
+            } else if (state.editingProp) {
+                let editingProp = { ...state.editingProp }
+                if (action.meta.field === editingProp.name)
+                    editingProp = { ...editingProp, value: action.payload }
+                return {
+                    ...state,
+                    editingProp,
+                }
+            }
+            return state
+        case TYPES.OPEN_TASK:
+            return {
+                ...state,
+                openTask: action.task,
+                editingProp: null,
+                editingTask: null,
+            }
+        case TYPES.EDIT_TASK:
+            return {
+                ...state,
+                openTask: null,
+                editingProp: null,
+                editingTask: action.task,
+            }
+        case TYPES.CLOSE_TASK:
+            return {
+                ...state,
+                openTask: null,
+                editingTask: null,
+                isNew: false,
+            }
+        case TYPES.CHANGE_TASK:
+            const editingTask = {
+                ...state.editingTask,
+                [action.field]: action.payload,
+            }
+            return {
+                ...state,
+                editingTask,
+            }
+        case TYPES.UPDATE_TASK:
+            const updatedTasks = state.tasks.map(task => {
+                if (task.id === action.id) return { ...action.task }
+
+                return { ...task }
+            })
+
+            return {
+                ...state,
+                tasks: updatedTasks,
+                editingTask: null,
+                openTask: state.openTask ? { ...action.task } : null,
+            }
+        case TYPES.UPDATE_OPEN_TASK:
+            let openTask = { ...state.openTask }
+            const updatedOpenTasks = state.tasks.map(task => {
+                if (task.id === state.openTask.id) {
+                    openTask = {
+                        ...openTask,
+                        [action.field]: action.payload,
+                    }
+                    return openTask
+                }
+
+                return { ...task }
+            })
+
+            return {
+                ...state,
+                openTask,
+                tasks: updatedOpenTasks,
+            }
+        case TYPES.REMOVE_TASK:
+            let index = null
+            state.tasks.forEach((task, i) => {
+                if (task.id !== action.id) return
+
+                index = i
+            })
+            if (!index) return state
+
+            if (state.tasks.length > 1) {
+                return {
+                    ...state,
+                    tasks: [
+                        ...state.tasks.slice(0, index),
+                        ...state.tasks.slice(index + 1),
+                    ],
+                    openTask: null,
+                    editingTask: null,
+                }
+            }
+
+            return {
+                ...state,
+                tasks: [],
+            }
         default:
-            return state;
+            return state
     }
-};
+}
