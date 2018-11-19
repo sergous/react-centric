adfa4cd7f9261f7b9023c90d0e5976cbaea76da6
Sergei Smirnov <sergous@gmail.com>
2018-11-19 04:39:52 -0800

    Add ui navigation type to actiontypes

--- app/actions/ui.jsx
+++ app/actions/ui.jsx
@@ -1,4 +1,4 @@
-import { LOCATION_CHANGE } from 'react-router-redux'
+import { LOCATION_CHANGE } from '../actiontypes'
 
 export const locationChange = ({pathname}) => {
     return {
--- app/reducers/ui.jsx
+++ app/reducers/ui.jsx
@@ -1,4 +1,4 @@
-import { LOCATION_CHANGE } from 'react-router-redux'
+import { LOCATION_CHANGE } from '../actiontypes'
 
 export default function ui(state = {}, action) {
     switch(action.type) {
