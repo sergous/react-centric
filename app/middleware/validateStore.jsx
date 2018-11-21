a55e07d6e4f048fb1dbeec80f94d47c8fdf1e6e7
Sergei Smirnov <sergous@gmail.com>
2018-11-20 23:40:03 -0800

    Reset routing on rehydrate (#23)

--- app/middleware/validateStore.jsx
+++ app/middleware/validateStore.jsx
@@ -5,6 +5,8 @@ export const validateStoreMiddleware = store => next => action => {
         case REHYDRATE:
             const projects = action.payload.project.projects.filter( project => Boolean(project.id) )
             action.payload.project.projects = projects;
+
+            action.payload.routing = null;
             break;
         default:
             break;
